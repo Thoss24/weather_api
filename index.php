9cf28bd0dfcc50a4faaffde2dbddfef25a177ccf
Thoss24 <t.fogarty24@outlook.com>
2024-02-01 15:58:18 +0000

    refactoring to use tailwindcss

--- index.php
+++ index.php
@@ -3,20 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="index.css" ></link>
+    <!-- <link rel="stylesheet" type="text/css" href="index.css" ></link> -->
+    <link href="./output.css" rel="stylesheet"></link>
     <title>Document</title>
 </head>
 <body>
 
     <main>
-    <div id="header">
+    <div class="flex justify-center flex-col items-center gap-y-2" id="header">
     <h1>Weather Api</h1>
     
-    <div>
-        <form action="" id="get-weather-form">
+    <div class="w-full flex justify-center">
+        <form action="" id="get-weather-form" class="flex flex-col w-3/4 items-center justify-center gap-y-3">
             <label for="location_val">Enter your Location</label>
-            <input type="text" id="location_val">
-            <button type="submit" id="get-weather">Get Weather</button>
+            <input type="text" id="location_val" class="outline outline-1 outline-offset-1">
+            <button class="rounded-xl p-0.5 outline outline-1 outline-offset-1" type="submit" id="get-weather">Get Weather</button>
         </form>
     </div>
     </div>
