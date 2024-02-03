bd8bc6bac0143be775e9b9b854e428e56ee13815
Thoss24 <t.fogarty24@outlook.com>
2024-02-03 12:34:20 +0000

    finished refactoring to tailwind

--- requests.js
+++ requests.js
@@ -44,24 +44,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
         console.log(typeof result.weather[0].description)
 
-        if (result.weather[0].description.includes("rain")) {
-          $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/rain.jpg" + ")")
-        }
-        if (result.weather[0].description.includes("clear")) {
-          $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/clear_sky.jpg" + ")")
-        }
-        if (result.weather[0].description.includes("overcast")) {
-          $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/overcast.jpg" + ")")
-        }
-        if (result.weather[0].description.includes("mist")) {
-          $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/misty.jpg" + ")")
-        }
-        if (result.weather[0].description.includes("broken")) {
-          $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/broken_clouds.jpg" + ")")
-        }
-        if (result.weather[0].description.includes("clouds") && !result.weather[0].description.includes("overcast")) {
-          $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/clouds.jpg" + ")")
-        }
+        // if (result.weather[0].description.includes("rain")) {
+        //   $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/rain.jpg" + ")")
+        // }
+        // if (result.weather[0].description.includes("clear")) {
+        //   $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/clear_sky.jpg" + ")")
+        // }
+        // if (result.weather[0].description.includes("overcast")) {
+        //   $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/overcast.jpg" + ")")
+        // }
+        // if (result.weather[0].description.includes("mist")) {
+        //   $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/misty.jpg" + ")")
+        // }
+        // if (result.weather[0].description.includes("broken")) {
+        //   $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/broken_clouds.jpg" + ")")
+        // }
+        // if (result.weather[0].description.includes("clouds") && !result.weather[0].description.includes("overcast")) {
+        //   $('#header').css("background-image", "url(" + "http://localhost/weather_api/images/clouds.jpg" + ")")
+        // }
 
       },
     });
@@ -87,7 +87,7 @@ document.addEventListener("DOMContentLoaded", () => {
           const resultsExcludingCurrentSearch = result.slice(0, result.length-1) // exclude most recent search
 
           resultsExcludingCurrentSearch.map((search) => {
-            const shell = $(`<div class="history-item-shell">
+            const shell = $(`<div class="flex flex-col justify-center items-center shadow-black shadow-md rounded-xl p-1">
             <div>
               <img src="https://openweathermap.org/img/wn/${search.icon}.png">
              <p>${search.temp} &deg c</p>
@@ -123,7 +123,7 @@ document.addEventListener("DOMContentLoaded", () => {
         success: () => {
           getPreviousSearches(search.location)
             //console.log(search)
-            const shell = $(`<div class="search-item-shell">
+            const shell = $(`<div class="flex flex-col justify-center items-center shadow-black shadow-md rounded-xl p-1">
             <div>
               <img src="https://openweathermap.org/img/wn/${search.icon}.png">
              <p>${search.temp} &deg c</p>
